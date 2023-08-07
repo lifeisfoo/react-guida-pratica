172e73eaba0ad3c220bafa7e75620d6ffade4027
Alessandro Miliucci <lifeisfoo@gmail.com>
2023-08-07 12:58:20 +0200

    react-scripts 5.0.1 + modal

--- ch11/src/components/ListDeleteModal.js
+++ ch11/src/components/ListDeleteModal.js
@@ -1,17 +1,19 @@
 export default function ListDeleteModal({ message, onDelete, onCancel }) {
   return (
-    <div className="modal-dialog">
-      <div className="modal-content">
-        <div className="modal-body">
-          <p>{message}</p>
-        </div>
-        <div className="modal-footer">
-          <button className="btn btn-secondary" onClick={onCancel}>
-            Annulla
-          </button>
-          <button className="btn btn-danger" onClick={onDelete}>
-            Elimina
-          </button>
+    <div className="modal fade show" style={{ display: "block" }}>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-body">
+            <p>{message}</p>
+          </div>
+          <div className="modal-footer">
+            <button className="btn btn-secondary" onClick={onCancel}>
+              Annulla
+            </button>
+            <button className="btn btn-danger" onClick={onDelete}>
+              Elimina
+            </button>
+          </div>
         </div>
       </div>
     </div>
