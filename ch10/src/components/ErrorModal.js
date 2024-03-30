1717bb6a06f82020eb75a4cecae40f7e09474a09
Alessandro Miliucci <lifeisfoo@gmail.com>
2024-03-30 12:38:54 +0100

    Aggiunto wrapper modal cap 10 e 11

--- ch10/src/components/ErrorModal.js
+++ ch10/src/components/ErrorModal.js
@@ -1,14 +1,16 @@
 export default function ErrorModal({ message, onConfirm }) {
   return (
-    <div className="modal-dialog">
-      <div className="modal-content">
-        <div className="modal-body">
-          <p>{message}</p>
-        </div>
-        <div className="modal-footer">
-          <button className="btn btn-secondary" onClick={onConfirm}>
-            OK
-          </button>
+    <div className="modal fade show" style={{ display: "block" }}>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-body">
+            <p>{message}</p>
+          </div>
+          <div className="modal-footer">
+            <button className="btn btn-secondary" onClick={onConfirm}>
+              OK
+            </button>
+          </div>
         </div>
       </div>
     </div>
